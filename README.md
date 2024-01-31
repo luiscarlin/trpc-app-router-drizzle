478374dd5c74f7a0c8b0afce4d1581fa565f4503
Luis Carlin <luiscarlin@gmail.com>
2024-01-30 19:34:55 -0500

    install drizzle and get the app finihsed

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 - [YouTube Tutorial] -> https://www.youtube.com/watch?v=qCLV0Iaq9zU
 - [Sample Code](https://github.com/jherr/trpc-on-the-app-router)
+
+## Stack
+
+- NextJS
+- Tailwind CSS
+- tRPC
+- Drizzle
+
+## Useful commands
+
+```bash
+# create a migration
+pnpm drizzle-kit generate:sqlite
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { serverClient } from "./_trpc/serverClient";
+
 import TodoList from "./_components/TodoList";
 
-export default function Home() {
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  const todos = await serverClient.getTodos();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TodoList />
+    <main className="max-w-3xl mx-auto mt-5">
+      <TodoList initialTodos={todos} />
     </main>
   );
 }
